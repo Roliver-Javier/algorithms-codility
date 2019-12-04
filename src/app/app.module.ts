115d43ca858d44fe6a883d30c90640c26f00b4c7
Roliver-Javier <roliverjavier@gmail.com>
2019-12-04 02:16:22 -0400

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { HelloComponent } from './hello.component';
 import { BinaryGapComponent } from './binary-gap/binary-gap.component';
 import { OddOccurrencesInArrayComponent } from './odd-occurrences-in-array/odd-occurrences-in-array.component';
 import { CyclickRotationComponent } from './cyclick-rotation/cyclick-rotation.component';
+import { FrogJumpComponent } from './frog-jump/frog-jump.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, BinaryGapComponent, OddOccurrencesInArrayComponent, CyclickRotationComponent ],
+  declarations: [ AppComponent, HelloComponent, BinaryGapComponent, OddOccurrencesInArrayComponent, CyclickRotationComponent, FrogJumpComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/frog-jump/frog-jump.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-frog-jump',
+  templateUrl: './frog-jump.component.html',
+  styleUrls: ['./frog-jump.component.css']
+})
+export class FrogJumpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
