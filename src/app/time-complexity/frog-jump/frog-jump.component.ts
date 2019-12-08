60b1b35af6bc9e1778624a60d26e5b22de7cc597
Roliver-Javier <roliverjavier@gmail.com>
2019-12-08 05:27:20 -0400

    1 file modified, 1 added

--- src/app/time-complexity/frog-jump/frog-jump.component.ts
+++ src/app/time-complexity/frog-jump/frog-jump.component.ts
@@ -11,6 +11,24 @@ export class FrogJumpComponent implements OnInit {
 
   ngOnInit() {
     console.log('hola');
+    console.log(this.fastSolution(10,300,20));
+    console.log(this.slowSolution(10,300,20));
+  }
+
+  fastSolution(X,Y,D){
+    const distanceLeft = Y-X;
+    let jumps = distanceLeft / D;
+    if(jumps % D !== 0) jumps++;
+    return Math.floor(jumps);
+  }
+
+  slowSolution(X,Y,D){
+    let jumps = 0;
+    while(X < Y){
+      X = X + D;
+      jumps++;
+    }
+    return jumps
   }
 
 }
