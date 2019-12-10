aad79874f907f8a4387918cbd0228a1930bf311d
Roliver-Javier <roliverjavier@gmail.com>
2019-12-10 17:51:27 -0400

    refactoring project's folder structure!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { BinaryGapComponent } from './binary-gap/binary-gap.component';
-import { OddOccurrencesInArrayComponent } from './odd-occurrences-in-array/odd-occurrences-in-array.component';
-import { CyclickRotationComponent } from './cyclick-rotation/cyclick-rotation.component';
+import { BinaryGapComponent } from './iterations/binary-gap/binary-gap.component';
 import { FrogJumpComponent } from './time-complexity/frog-jump/frog-jump.component';
 import { PermMissingElemComponent } from './time-complexity/perm-missing-elem/perm-missing-elem.component';
+import { CyclickRotationComponent } from './arrays/cyclick-rotation/cyclick-rotation.component';
+import { OddOccurrencesInArrayComponent } from './arrays/odd-occurrences-in-array/odd-occurrences-in-array.component';
+
+
+const algorithms = {
+  iterations: [ 
+    BinaryGapComponent 
+  ],
+  arrays: [ 
+    OddOccurrencesInArrayComponent, 
+    CyclickRotationComponent 
+  ],
+  timeComplexity: [ 
+    FrogJumpComponent, 
+    PermMissingElemComponent 
+  ]
+};
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, BinaryGapComponent, OddOccurrencesInArrayComponent, CyclickRotationComponent, FrogJumpComponent, PermMissingElemComponent ],
+  declarations: [ AppComponent,
+  ...algorithms.iterations,
+  ...algorithms.arrays,  
+  ...algorithms.timeComplexity
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
