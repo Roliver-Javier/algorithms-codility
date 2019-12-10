abdf155042f9d1df26a6d5aad58d5a8d16d79592
Roliver-Javier <roliverjavier@gmail.com>
2019-12-10 18:00:42 -0400

    testing-bug

---
+++ src/app/testing-bug/testing-bug.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testing-bug',
+  templateUrl: './testing-bug.component.html',
+  styleUrls: ['./testing-bug.component.css']
+})
+export class TestingBugComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
