25a1793a5954e1a4d1631d1eb1a708ab1fa8920c
Roliver-Javier <roliverjavier@gmail.com>
2019-12-12 03:25:12 -0400

    tape-equilibrium algorithm - 53%

--- src/app/time-complexity/tape-equilibrium/tape-equilibrium.component.ts
+++ src/app/time-complexity/tape-equilibrium/tape-equilibrium.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tape-equilibrium',
-  templateUrl: './tape-equilibrium.component.html',
+  template: '',
   styleUrls: ['./tape-equilibrium.component.css']
 })
 export class TapeEquilibriumComponent implements OnInit {
@@ -10,26 +10,40 @@ export class TapeEquilibriumComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.Solution([3,1,2,4,3]);
+    console.log(this.Solution([1,2]));
   }
+/*
+[1][2] = 1 - 2 = -1
+
+
+[1][1,2] = 1 - 3 = -2
+[1,1][2] = 2 - 2 = 0
+
+[5][6,2,4,1] = 5 - 13 = -8
+[5,6][2,4,1] = 11 - 7 = 4
+[5,6,2][4,1] = 13 - 5 = 8
+[5,6,2,4][1] = 17 - 1 = 16
+
+*/
 
   Solution(A: number[]){
-    let arr1 = [], arr2 = [];
-    let copyArr = A;
-    for(let i = 1; i < A.length; i++){
-     arr1.push(...copyArr.splice(0,i));
-     console.log(i);
-     console.log(arr1);
-     console.log(copyArr);
-      copyArr = A;
+    let arr1 = [], results = [], i = 0, copyArr = A;
+    const length = A.length;
+    while(i < length - 1){
+      arr1.push(...copyArr.splice(0,1));
+      let arr1Sum = this.sumArrElem(arr1);
+      let arr2Sum = this.sumArrElem(copyArr);
+      results.push( Math.abs(arr1Sum - arr2Sum));
+      i++;
     }
-
-    // console.log(copyArr.splice(0,1));
-    // console.log(copyArr.splice(0,2));
-    // console.log(copyArr.splice(0,1));
-  
+    console.log(results);
+    return this.getMinimalElem(results);
   }
 
+  getMinimalElem(A: any[]){
+    return A.sort((a,b)=> a < b? -1 : 1)[0];
+  }
+  
   sumArrElem(A: any[]){
     return A.reduce((a,b)=> a + b);
   }
